0c7e2def3e2befa20ab5c4c303ba8508dd969caa
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2023-10-16 11:55:05 +0200

    test: :white_check_mark: add misconfiguration test

--- pkg/consent/mapper_test.go
+++ pkg/consent/mapper_test.go
@@ -1,86 +1,138 @@
 package consent
 
 import (
+	"errors"
 	"github.com/samply/golang-fhir-models/fhir-models/fhir"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
+type Expected struct {
+	result *DomainStatus
+	error  error
+}
+
+type ParseConsentTestCase struct {
+	name     string
+	domain   Domain
+	policies []fhir.Consent
+	expected Expected
+}
+
 func TestParseConsent(t *testing.T) {
 	// prepare dates
 	now := time.Now().UTC()
 	now = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(),
 		now.Minute(), now.Second(), 0, now.Location())
-	// policy consent resources
-	policy1, _ := fhir.Consent{
-		Meta: &fhir.Meta{
-			LastUpdated: of("2023-09-28T00:00:00+02:00"),
-		},
-		Provision: of(fhir.ConsentProvision{
-			Type: of(fhir.ConsentProvisionTypePermit),
-			Period: &fhir.Period{
-				Start: of(now.Format(time.RFC3339)),
-				End:   of(now.AddDate(5, 0, 0).Format(time.RFC3339)),
-			},
-			Code: []fhir.CodeableConcept{{
-				Coding: []fhir.Coding{{
-					System: of("https://ths-greifswald.de/fhir/CodeSystem/gics/Policy/MII"),
-					Code:   of("MDAT_erheben"),
-				}},
-			}},
-		}),
-	}.MarshalJSON()
-	policy2, _ := fhir.Consent{
-		Meta: &fhir.Meta{
-			LastUpdated: of(now.Format(time.RFC3339)),
-		},
-		Provision: of(fhir.ConsentProvision{
-			Type: of(fhir.ConsentProvisionTypeDeny),
-			Period: &fhir.Period{
-				Start: of(now.Format(time.RFC3339)),
-				End:   of(now.AddDate(10, 0, 0).Format(time.RFC3339)),
-			},
-			Code: []fhir.CodeableConcept{{
-				Coding: []fhir.Coding{{
-					System: of("https://ths-greifswald.de/fhir/CodeSystem/gics/Policy/MII"),
-					Code:   of("MDAT_speichern_verarbeiten"),
-				}},
-			}},
-		}),
-	}.MarshalJSON()
 
-	bundle := &fhir.Bundle{Entry: []fhir.BundleEntry{
+	// test cases
+	cases := []ParseConsentTestCase{
 		{
-			Resource: policy1,
+			name: "parseConsent_Success",
+			domain: Domain{
+				Name:            "Test",
+				Description:     "Test domain",
+				CheckPolicyCode: "MDAT_erheben",
+				PersonIdSystem:  "Patient-ID",
+			},
+			// policy consent resources
+			policies: getTestConsentPolicies(now),
+			expected: Expected{
+				&DomainStatus{
+					Domain:      "Test",
+					Description: "Test domain",
+					Status:      "accepted",
+					LastUpdated: &now,
+					Expires:     of(now.AddDate(5, 0, 0)),
+					AskConsent:  false,
+					Policies: []Policy{
+						{"MDAT_erheben", true, "MDAT_erheben"},
+						{"MDAT_speichern_verarbeiten", false, "MDAT_speichern_verarbeiten"},
+					},
+				}, nil,
+			},
 		},
 		{
-			Resource: policy2,
-		},
-	}}
-	// target domain
-	domain := Domain{
-		Name:            "Test",
-		Description:     "Test domain",
-		CheckPolicyCode: "MDAT_erheben",
-		PersonIdSystem:  "Patient-ID",
-	}
-
-	res, _ := ParseConsent(bundle, domain)
-
-	expected := DomainStatus{
-		Domain:      "Test",
-		Description: "Test domain",
-		Status:      "accepted",
-		LastUpdated: &now,
-		Expires:     of(now.AddDate(5, 0, 0)),
-		AskConsent:  false,
-		Policies: []Policy{
-			{"MDAT_erheben", true, "MDAT_erheben"},
-			{"MDAT_speichern_verarbeiten", false, "MDAT_speichern_verarbeiten"},
+			name: "parseConsent_FailsWithInvalidCheckPolicy",
+			domain: Domain{
+				Name:            "Test",
+				Description:     "Test domain",
+				CheckPolicyCode: "#MDAT_erheben#",
+				PersonIdSystem:  "Patient-ID",
+			},
+			// policy consent resources
+			policies: getTestConsentPolicies(now),
+			expected: Expected{
+				nil,
+				errors.New("checkPolicy not found for domain"),
+			},
 		},
 	}
-	assert.Equal(t, expected, *res)
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			parseConsentHandler(t, c)
+		})
+	}
+}
+
+func getTestConsentPolicies(from time.Time) []fhir.Consent {
+	return []fhir.Consent{
+		{
+			Meta: &fhir.Meta{
+				LastUpdated: of(from.Format(time.RFC3339)),
+			},
+			Provision: of(fhir.ConsentProvision{
+				Type: of(fhir.ConsentProvisionTypePermit),
+				Period: &fhir.Period{
+					Start: of(from.Format(time.RFC3339)),
+					End:   of(from.AddDate(5, 0, 0).Format(time.RFC3339)),
+				},
+				Code: []fhir.CodeableConcept{{
+					Coding: []fhir.Coding{{
+						System: of("https://ths-greifswald.de/fhir/CodeSystem/gics/Policy/MII"),
+						Code:   of("MDAT_erheben"),
+					}},
+				}},
+			}),
+		},
+		{
+			Meta: &fhir.Meta{
+				LastUpdated: of(from.Format(time.RFC3339)),
+			},
+			Provision: of(fhir.ConsentProvision{
+				Type: of(fhir.ConsentProvisionTypeDeny),
+				Period: &fhir.Period{
+					Start: of(from.Format(time.RFC3339)),
+					End:   of(from.AddDate(10, 0, 0).Format(time.RFC3339)),
+				},
+				Code: []fhir.CodeableConcept{{
+					Coding: []fhir.Coding{{
+						System: of("https://ths-greifswald.de/fhir/CodeSystem/gics/Policy/MII"),
+						Code:   of("MDAT_speichern_verarbeiten"),
+					}},
+				}},
+			}),
+		},
+	}
+}
+
+func parseConsentHandler(t *testing.T, c ParseConsentTestCase) {
+
+	entries := make([]fhir.BundleEntry, 0)
+	for _, p := range c.policies {
+		r, _ := p.MarshalJSON()
+		entries = append(entries, fhir.BundleEntry{Resource: r})
+	}
+
+	bundle := &fhir.Bundle{Entry: entries}
+
+	// act
+	res, err := ParseConsent(bundle, c.domain)
+
+	assert.Equal(t, c.expected.result, res)
+	assert.Equal(t, c.expected.error, err)
 }
 
 type ParsePolicyTestCase struct {
